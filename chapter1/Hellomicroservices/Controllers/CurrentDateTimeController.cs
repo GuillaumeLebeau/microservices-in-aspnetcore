a627887cdbc814b68d295966eff7a3e92a4bffd0
Guillaume Lebeau <guillaume.lebeau@irec.fr>
2018-05-28 12:31:43 +0200

    1.8.3 Simple GET endpoint

---
+++ chapter1/Hellomicroservices/Controllers/CurrentDateTimeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hellomicroservices.Controllers
+{
+    [Route("/api/[controller]")]
+    public class CurrentDateTimeController : Controller
+    {
+        [HttpGet]
+        public DateTime Get()
+        {
+            return DateTime.UtcNow;;
+        }
+    }
+}
